cecb59d462444edc1c922f15f04a753172426a5d
HiagoSalvador <hiago.salvador@hotmail.com>
2024-09-19 14:48:10 -0300

    fixing-bugs

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,10 +5,6 @@
         <img src="../assets/logo.png" alt="Logo do Cookin' Up" class="logo">
       </a>
    
-
-
-  
-        
         <p class="cabecalho-lg frase-cabecalho">
           <span class="texto-verde">Um banquete de ideias para</span>
           despertar o chef que há em você!
@@ -20,7 +16,6 @@
         </p>
       </div>
   
-      <!-- Imagem com a classe 'foto-banner' -->
       <img src="../assets/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e o ombro" class="foto-banner">
 
     
